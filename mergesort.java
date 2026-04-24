eb8e8ea34b07bebdcfd1fbfea0fa019a47c08310
nandinigarg0404 <nandinigarg0406@gmail.com>
2026-04-24 22:30:44 +0530

    Update mergesort.java
    mergesort

--- mergesort.java
+++ mergesort.java
@@ -1,40 +1,45 @@
-class mergesort{
-    public static void divide(int[] arr,int si, int ei){
-        if(si>=ei){
+
+class mergesort {
+
+    public static void divide(int[] arr, int si, int ei) {
+        if (si >= ei) {
             return;
         }
-        int mid=si+(ei-si)/2;
+        int mid = si + (ei - si) / 2;
         divide(arr, si, mid);
-        divide(arr, mid+1, ei);
+        divide(arr, mid + 1, ei);
         merge(arr, si, ei, mid);
     }
-    public static void merge(int[] arr,int si,int ei,int mid){
-        int[] merged=new int[ei-si+1];
-        int idx1=si;
-        int idx2=mid+1;
-        int x=0;
-        while(idx1<=mid&&idx2<=ei){
-            if(arr[idx1]<=arr[idx2]){
-                merged[x++]=arr[idx1++];
+
+    public static void merge(int[] arr, int si, int ei, int mid) {
+        int[] merged = new int[ei - si + 1];
+        int idx1 = si;
+        int idx2 = mid + 1;
+        int x = 0;
+        while (idx1 <= mid && idx2 <= ei) {
+            if (arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
+            } else {
+                merged[x++] = arr[idx2++];
             }
-            else{
-                merged[x++]=arr[idx2++];
-            }
         }
-        while(idx1<=mid){
-            merged[x++]=arr[idx1++];
+        while (idx1 <= mid) {
+            merged[x++] = arr[idx1++];
         }
-        while(idx2<=ei){
-            merged[x++]=arr[idx2++];
+        while (idx2 <= ei) {
+            merged[x++] = arr[idx2++];
         }
-        for(int i=0,j=si;i<merged.length;j++,i++){
-            //System.out.print(merged[i]+" ");
-            arr[j]=merged[i];
-            System.out.print(arr[j]+" ");
+        for (int i = 0, j = si; i < merged.length; j++, i++) {
+            arr[j] = merged[i];
         }
+
     }
+
     public static void main(String[] args) {
-        int[] arr={70,30,50,10};
-        divide(arr,0,arr.length);
+        int[] arr = {70, 30, 50, 10};
+        divide(arr, 0, arr.length - 1);
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
     }
 }
