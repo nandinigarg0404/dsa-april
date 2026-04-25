7236e3d0ef9b955be47a3066c3a09e78cbe75326
nandinigarg0404 <nandinigarg0406@gmail.com>
2026-04-25 17:51:50 +0530

    Update circularqueue.java

--- circularqueue.java
+++ circularqueue.java
@@ -20,6 +20,28 @@ class circqueue {
         System.out.println(elememts+" inserted");
 
     }
+    public void display(){
+        if(front==-1){
+            System.out.println("queue is empty");
+        }
+        int i=front;
+        /*while(i!=rear+1){
+            System.out.print(queue[i]+" ");  \\gives infinite 
+            i=(i+1)%(size-1);
+        }
+        System.out.println();*/
+        do { 
+            System.out.println(queue[i]+" ");
+            i=(i+1)%size;
+        } while (i!=(rear+1)%size);
+    }
+    public void dequeue(){
+        if(front==-1){
+            System.out.println("queue is empty");
+        }
+        front=(front+1)%size;
+        System.out.println("element is dequeued");
+    }
 }
 class circularqueue{
     public static void main(String[] args) {
@@ -28,5 +50,11 @@ class circularqueue{
         cobj.enqueue(10);
         cobj.enqueue(15);
         cobj.enqueue(20);
+        cobj.enqueue(25);
+        cobj.enqueue(30);
+        cobj.display();
+        cobj.dequeue();
+        cobj.dequeue();
+        cobj.display();
     }
 }
