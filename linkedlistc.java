0fe9c6e4c0f2eb125dc1e579613f37b43622f877
nandinigarg0404 <nandinigarg0406@gmail.com>
2026-05-01 18:08:20 +0530

    Update linkedlistc.java
    iohyg

--- linkedlistc.java
+++ linkedlistc.java
@@ -14,8 +14,25 @@ class link {
 
     Node head;
 
-    void insertc(int data) { //at centre
-        Node newnode = new Node(data);
+    void insertm(int position, int data) {  //insert at any specific position
+        Node newnNode = new Node(data);
+        if (position == 0) {
+            newnNode.next = head;
+            head = newnNode;
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < position - 1; i++) {
+            if (temp == null) {
+                return;
+            }
+            temp = temp.next;
+        }
+        if (temp == null) {
+            return;
+        }
+        newnNode.next = temp.next;
+        temp.next = newnNode;
     }
 
     void insertb(int data) {
@@ -47,12 +64,33 @@ class link {
     }
 
     void removeb() { //remove from beginning
-        if(head==null){
+        if (head == null) {
             return;
         }
         Node temp = head;
         head = temp.next;
         temp.next = null;
+    }
+
+    void removem(int position) {
+        Node temp = head;
+        if (head == null) {
+            return;
+        }
+        if (position == 0) {
+            head = temp.next;
+            temp.next = null;
+            return;
+        }
+        for (int i = 0; i < position - 1; i++) {
+            if (temp==null||temp.next == null) {
+                return;
+            }
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
     }
 
     void display() {
@@ -69,14 +107,15 @@ class linkedlistc {
 
     public static void main(String[] args) {
         link lst = new link();
-        lst.insertc(10);
         lst.insertb(20);
         lst.insertb(30);
         lst.inserte(40);
         lst.inserte(50);
+        lst.insertm(3, 45);
         lst.display();
-        lst.removel();
-        lst.removeb();
+        lst.removem(4);
+        //lst.removel();
+        //lst.removeb();
         lst.display();
     }
 }
