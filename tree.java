47f88f5c455862f71b243a7ecd2506d4aaaaa68e
nandinigarg0404 <nandinigarg0406@gmail.com>
2026-05-06 18:07:18 +0530

    Update tree.java
    sum of nodes

--- tree.java
+++ tree.java
@@ -1,35 +1,42 @@
-class Tree{
+
+class Tree {
+
     int data;
-    Tree left,right;
-    Tree(int data){
-        this.data=data;
-        left=right=null;
+    Tree left, right;
+
+    Tree(int data) {
+        this.data = data;
+        left = right = null;
     }
 
 }
-class tree{
-    public static void print(Tree root){
-        if(root==null){
+
+class tree {
+
+    public static void print(Tree root) {
+        if (root == null) {
             return;
         }
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         print(root.left);
         print(root.right);
     }
-    /*void sum(Tree root){
-        int sum=0;
-        if(root==null){
-            return;
-        }
-        sum+=root.data;
 
-    }*/
+    static int sum(Tree root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.data + sum(root.left) + sum(root.right);
+    }
+
     public static void main(String[] args) {
-        Tree root=new Tree(10);
-        root.left=new Tree(20);
-        root.left.left=new Tree(30);
-        root.left.right=new Tree(40);
-        root.right=new Tree(50);
+        Tree root = new Tree(10);
+        root.left = new Tree(20);
+        root.left.left = new Tree(30);
+        root.left.right = new Tree(40);
+        root.right = new Tree(50);
         print(root);
+        System.out.println("sum of nodes: " + sum(root));
+
     }
 }
